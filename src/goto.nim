e450eb58ed653c9a445c1589a96aae0908ac65e8
huakim <92579808+huakim@users.noreply.github.com>
2024-06-09 14:07:37 +0300

    Add 'NIMROD_LABEL_NAME_' prefix, add label without body

--- src/goto.nim
+++ src/goto.nim
@@ -1,17 +1,19 @@
-import std/macros
-
 macro label*(labelName, body: untyped): untyped =
-  expectKind(labelName, nnkIdent)
-  let name = repr(labelName)
-  result = quote do:
-    {.emit: `name` & ":".}
-    `body`
+    expectKind(labelName, nnkIdent)
+    let name = "NIMROD_LABEL_NAME_" & repr(labelName)
+    result = quote do:
+        {.emit: `name` & ":".}
+        `body`
 
 macro goto*(labelName: untyped): untyped =
-  expectKind(labelName, nnkIdent)
-  let name = repr(labelName)
-  result = quote do:
-    {.emit: "goto " & `name` & ";".}
+    expectKind(labelName, nnkIdent)
+    let name = "NIMROD_LABEL_NAME_" & repr(labelName)
+    result = quote do:
+        {.emit: "goto " & `name` & ";".}
+
+template label*(labelName: untyped): untyped =
+    label labelName:
+        discard
 
 template jmp*(labelName: untyped): untyped =
-  goto labelName
+    goto labelName
